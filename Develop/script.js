3be4c6d760b2a6c5ee36f716061d5ae2e3dcfb77
Taryn Moore <tarynmoore628@gmail.com>
2022-05-18 14:22:27 -0600

    Got the local storage working. Worked with my tutor to figure out how to get it in a loop instead of doing the local storage line by line.

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,14 @@
 var today = moment();
 $('#currentDay').text(today.format ("MMM Do, YYYY"));
 
+//Save the user text into local storage
+$(".saveBtn").on('click', function (event) {
+    event.preventDefault();
+    var userText = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, userText);
+})
+
 // Sets past,present and future classes to the time. 
 function timeIndicator() {
 var currentTime = moment().hour() 
@@ -9,7 +17,10 @@ var timeBlock = $('.time-block')
 
 timeBlock.each(function() {
     var hour = parseInt($(this).attr("id"));
-
+    
+    var time = $(this).attr("id")
+    console.log("#"+ time + " textarea");
+    $("#"+ time + " textarea").val(localStorage.getItem(time))
     if (hour === currentTime) {
         $(this).addClass("present");
     } else if (hour < currentTime) {
@@ -21,19 +32,4 @@ timeBlock.each(function() {
 }
 timeIndicator()
 
-$(".saveBtn").on('click', function (event) {
-    event.preventDefault();
-    var userText = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, userText);
-})
 
-$("#9am textarea").val(localStorage.getItem("9am"));
-$("#10am textarea").val(localStorage.getItem("10am"));
-$("#11am textarea").val(localStorage.getItem("11am"));
-$("#12pm textarea").val(localStorage.getItem("12pm"));
-$("#13pm textarea").val(localStorage.getItem("13pm"));
-$("#14pm textarea").val(localStorage.getItem("14pm"));
-$("#15pm textarea").val(localStorage.getItem("15pm"));
-$("#16pm textarea").val(localStorage.getItem("16pm"));
-$("#17pm textarea").val(localStorage.getItem("17pm"));
